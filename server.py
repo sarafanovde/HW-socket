0a6178c57c8836e6738dd4353af7799c7110d48b
sarafanovde <Sarafanovde@yandex.ru>
2016-03-26 00:47:39 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, os
+import socket,os
 
 HOST = 'localhost'
 PORT = 8000
@@ -10,18 +10,16 @@ print ('Server started on port %s ...'%(PORT))
 while True:
  	connection, address = sock.accept()
  	data = connection.recv(1024).decode()
- 	print(data)
- 	res = data.split('\n')[0].split(' ')[1]
- 	path = './' + res 
-	    
- 	if not os.path.isfile(path):
-	 	path ='./index.html' 
-		    
+ 	res = data.split('\n')[0].split(' ')[1] 
+ 	print(res)
+ 	if res == '/' or res == '/index.html':
+ 		path = 'index.html'
+ 	if res == '/about/aboutme.html':
+ 		path = 'about/aboutme.html'	    
  	file = open(path, 'rb')
  	connection.send("""HTTP/1.1 200 OK\n\n\n""".encode() + file.read())
  	connection.send(file.read())
  	file.close()
  	connection.close()    
 sock.close()
-
 	
